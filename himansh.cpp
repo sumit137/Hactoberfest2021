a57a7255f9e1746dfde89bc0f71f2397da21a996
sumit137 <85798618+sumit137@users.noreply.github.com>
2021-10-24 11:57:06 +0530

    Create himansh.cpp

---
+++ himansh.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstdio>
+#include<iomanip>
+using namespace std;
+
+int main() {
+   int a;
+   long b;
+   char c;
+   float d;
+   double e;
+   cin>>a>>b>>c>>d>>e;
+   cout<<a<<endl<<b<<endl<<c<<endl<<fixed<<setprecision(2)<<d<<endl<<e;
+    return 0;
+}
